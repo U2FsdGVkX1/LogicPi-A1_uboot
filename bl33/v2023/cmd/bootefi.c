54177cdbbbb7a77f0e7696a6596ec99cc55bff89
U2FsdGVkX1 <U2FsdGVkX1@gmail.com>
2026-07-31 18:37:40 +0800

    cmd: bootefi: Prepare Amlogic reserved memory
    When bootefi is given an external device tree, it installs that tree
    without running the Amlogic reserved-memory and memory-tag checks used
    by bootm. Amlogic EFI boots therefore miss the platform-specific memory
    preparation.
    
    For Amlogic builds, set the working FDT and run rsvmem check and
    memtag check before installing an external device tree. Propagate
    rsvmem failures to the command caller.
    
    Signed-off-by: U2FsdGVkX1 <U2FsdGVkX1@gmail.com>

--- bl33/v2023/cmd/bootefi.c
+++ bl33/v2023/cmd/bootefi.c
@@ -16,6 +16,7 @@
 #include <efi_selftest.h>
 #include <env.h>
 #include <errno.h>
+#include <fdt_support.h>
 #include <image.h>
 #include <log.h>
 #include <malloc.h>
@@ -684,6 +685,28 @@ static int do_bootefi(struct cmd_tbl *cmdtp, int flag, int argc,
 	} else {
 		fdt = EFI_FDT_USE_INTERNAL;
 	}
+
+#ifdef CONFIG_AMLOGIC_MODIFY
+	/* Apply the same platform memory setup used by the bootm/booti path. */
+	if (fdt != EFI_FDT_USE_INTERNAL && !fdt_check_header(fdt)) {
+		ulong fdt_addr = map_to_sysmem(fdt);
+
+		if (CONFIG_IS_ENABLED(CMD_FDT))
+			set_working_fdt_addr(fdt_addr);
+
+#ifdef CONFIG_CMD_RSVMEM
+		ret = run_command("rsvmem check", 0);
+		if (ret) {
+			log_err("rsvmem check failed\n");
+			return CMD_RET_FAILURE;
+		}
+#endif
+
+#ifdef CONFIG_CMD_MEMTAG
+		run_command("memtag check", 0);
+#endif
+	}
+#endif
 	ret = efi_install_fdt(fdt);
 	if (ret == EFI_INVALID_PARAMETER)
 		return CMD_RET_USAGE;
